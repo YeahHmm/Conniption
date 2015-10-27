aac2be8be444d25896fb24f04e1a71d2c85bb328
armandosrz <armandosrz@users.noreply.github.com>
2015-10-27 18:36:51 -0500

    Update printFunction.py
    Finished

--- printFunction.py
+++ printFunction.py
@@ -5,8 +5,8 @@ def printma():
     toPrint = ''
     for i, j in enumerate(matrix):
         toPrint += '{0:^7}|'.format(j)
-        if i == 7 or i == 14 or i == 21 or i ==28 or i == 35\
-           or i ==42:
+        if i == 6 or i == 13 or i == 20 or i ==27 or i == 34\
+           or i ==41:
             toPrint += '\n'
     print (toPrint)
 if __name__ == "__main__":
