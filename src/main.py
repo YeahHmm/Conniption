f09c1eca28c64dd135cf42147e16b7650afdfb2e
lamb <trotter.justin@gmail.com>
2015-11-02 15:47:22 -0600

    set up PrintState

--- src/main.py
+++ src/main.py
@@ -4,6 +4,13 @@ from printFunction import printboard
 from resource import SystemState
 from resource import Move
 
+def printState(state, prompt=''):
+	os.system('clear')
+	print (state)
+	if prompt != '':
+		print(prompt)
+
+
 def main():
 	cur_state = SystemState()
 	valid = False
@@ -11,17 +18,17 @@ def main():
 	is_goal = False
 	winner = 2
 	while not is_goal:
-		os.system('clear')
-		print (cur_state)
-		print(prevMv, valid)
-		print(cur_state.genMoves())
 		for i in range (1,4):
 			if i != 2:
-				val = input("input action (flip/none): ").strip().split(' ')
+				os.system('clear')
+				printState (cur_state, "input action (flip/none): ")
+				val = input().strip().split(' ')
 				key = val[0]
 				pos = -1
 			else:
-				val = input("input action (place [1-7]): ").strip().split(' ')
+				os.system('clear')
+				printState (cur_state, "input action (place [1-7]): ")
+				val = input().strip().split(' ')
 				key = 'place'
 				pos = int(val[-1]) - 1
 			move = Move(key, cur_state._cur_player, pos)
@@ -32,8 +39,7 @@ def main():
 				is_goal, winner = cur_state.isGoal()
 				if is_goal:
 					break
-			os.system('clear')
-			print (cur_state)
+
 
 	os.system('clear')
 	print (cur_state)
